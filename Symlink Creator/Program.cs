25bfbd507f5c13ee07702cbb7ffe2b857093be86
amd989 <alejandro@shift-me.com>
2015-04-22 17:53:06 -0600

    Switched to git from subversion

---
+++ Symlink Creator/Program.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="ShiftMe, Inc.">
+//   2010-2013
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Symlink_Creator
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>The program.</summary>
+    internal static class Program
+    {
+        #region Methods
+
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+
+        #endregion
+    }
+}
